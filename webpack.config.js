24bef421ca6cdb403fae1626b5ff2733990c15c1
Rafael Madureira dos Santos <rafael.madureira.santos@gmail.com>
2019-07-13 15:34:53 -0300

    Update all dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
@@ -19,7 +19,7 @@ module.exports = {
     ]
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin(),
     new CopyWebpackPlugin([{
       from: 'resources/',
       to: './',
